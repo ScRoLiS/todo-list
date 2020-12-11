ccf647be4135d892f0b549555025280b454569a4
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2020-12-11 19:32:06 +0600

    Remove data state

--- src/hoc/withInput.js
+++ src/hoc/withInput.js
@@ -3,15 +3,16 @@ import React, { useState } from 'react';
 function withInput(Component) {
   return function (props) {
     const [value, setValue] = useState(props.value);
-    const [data, setData] = useState(props.data);
 
     const onChange = (e) => {
       setValue(e.target.value);
     };
 
-    const action = () => {
-      props.action({ value, data });
-      setValue('');
+    const action = (data) => {
+      if(value) {
+        props.action({ value, data });
+        setValue('');
+      } else alert('Пустое нельзя');
     };
 
     return (
