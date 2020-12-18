4458eb1fd6ced86d575ec83c80a1ad28bb309651
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2020-12-18 16:21:34 +0600

    Remove list function

--- src/utils/Todo.js
+++ src/utils/Todo.js
@@ -1,9 +1,14 @@
 export default class Todo {
+
+    static newList(id, icon, name, todos) {
+        return { id, icon, name, todos };
+    }
+
     static newTodo(id, name, done, important) {
         return { id, name, done, important };
     }
 
-    static newList(id, icon, name, todos) {
-        return { id, icon, name, todos };
+    static removeList(data, id) {
+        return data.filter(item => item.id !== id);
     }
 }
