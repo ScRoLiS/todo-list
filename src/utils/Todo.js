bd5e78696603471162e20b479d96255eeef76ebe
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2020-12-18 21:16:55 +0600

    getList function

--- src/utils/Todo.js
+++ src/utils/Todo.js
@@ -42,4 +42,11 @@ export default class Todo {
 	static removeList(data, id) {
 		return data.filter(item => item.id !== id);
 	}
+
+	static getList(data, id) {
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].id === id)
+				return data[i];
+		}
+	}
 }
