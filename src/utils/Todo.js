ba24e2f2adfca24e1ddd51e5f45ef55de15a7f7d
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2020-12-18 16:41:04 +0600

    Todos preset

--- src/utils/Todo.js
+++ src/utils/Todo.js
@@ -1,14 +1,45 @@
 export default class Todo {
 
-    static newList(id, icon, name, todos) {
-        return { id, icon, name, todos };
-    }
+	static rnd() {
+		return Math.random();
+	}
 
-    static newTodo(id, name, done, important) {
-        return { id, name, done, important };
-    }
+	static todos = [
+		Todo.newList(this.rnd(), 'home', 'First List', [
+			Todo.newTodo(this.rnd(), '1.1 Todo', false, false),
+			Todo.newTodo(this.rnd(), '1.2 Todo', false, false),
+			Todo.newTodo(this.rnd(), '1.3 Todo', false, false),
+		]),
+		Todo.newList(this.rnd(), 'star', 'Second List', [
+			Todo.newTodo(this.rnd(), '2.1 Todo', false, false),
+			Todo.newTodo(this.rnd(), '2.2 Todo', false, false),
+			Todo.newTodo(this.rnd(), '2.3 Todo', false, false),
+		]),
+		Todo.newList(this.rnd(), 'cash', 'Third List', [
+			Todo.newTodo(this.rnd(), '3.1 Todo', false, false),
+			Todo.newTodo(this.rnd(), '3.2 Todo', false, false),
+			Todo.newTodo(this.rnd(), '3.3 Todo', false, false),
+		]),
+		Todo.newList(this.rnd(), 'create', 'Fourth List', [
+			Todo.newTodo(this.rnd(), '4.1 Todo', false, false),
+			Todo.newTodo(this.rnd(), '4.2 Todo', false, false),
+			Todo.newTodo(this.rnd(), '4.3 Todo', false, false),
+		]),
+	];
 
-    static removeList(data, id) {
-        return data.filter(item => item.id !== id);
-    }
+	static newList(id, icon, name, todos) {
+		return { id, icon, name, todos };
+	}
+
+	static newTodo(id, name, done, important) {
+		return { id, name, done, important };
+	}
+
+	static addList(data, list) {
+		return [...data, list];
+	}
+
+	static removeList(data, id) {
+		return data.filter(item => item.id !== id);
+	}
 }
